eaf545f4df0e64211900a5928f1492eef770dcb4
nikitamavrodiy <106612451+nikitamavrodiy@users.noreply.github.com>
2025-12-27 22:25:28 +0200

    Add arg validation and basic file opening logic

--- cmpe351.c
+++ cmpe351.c
@@ -11,5 +11,24 @@ main(int argc, char *argv[])
         return 1;
     }
 
+    FILE *in = fopen(argv[1], "r");
+    if (!in) {
+        perror("Input file");
+        return 1;
+    }
+
+    FILE *out = fopen(argv[2], "w");
+    if (!out) {
+        perror("Output file");
+        fclose(in);
+        return 1;
+    }
+
+    /* Temporary message */
+    fprintf(out, "OK\n");
+
+    fclose(in);
+    fclose(out);
+
     return 0;
 }
