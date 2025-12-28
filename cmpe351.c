6ec23ef5939a023fa037a773b3ed61667305fee3
nikitamavrodiy <106612451+nikitamavrodiy@users.noreply.github.com>
2025-12-28 23:37:04 +0200

    Add input parsing and process linked list structure

--- cmpe351.c
+++ cmpe351.c
@@ -3,6 +3,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct process {
+    int burst;
+    int priority;
+    int arrival;
+    int queue_id;
+    struct process *next;
+};
+
+struct process *
+read_input(FILE *in)
+{
+    struct process *head = NULL;
+    struct process *tail = NULL;
+
+    int burst, priority, arrival, queue_id;
+
+    while (fscanf(in, "%d:%d:%d:%d",
+                  &burst, &priority, &arrival, &queue_id) == 4) {
+
+        struct process *p = malloc(sizeof(struct process));
+        if (!p) {
+            perror("malloc");
+            return head;
+        }
+
+        p->burst = burst;
+        p->priority = priority;
+        p->arrival = arrival;
+        p->queue_id = queue_id;
+        p->next = NULL;
+
+        if (!head)
+            head = p;
+        else
+            tail->next = p;
+
+        tail = p;
+    }
+
+    return head;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -24,11 +66,17 @@ main(int argc, char *argv[])
         return 1;
     }
 
-    /* Temporary message */
-    fprintf(out, "OK\n");
+    struct process *plist = read_input(in);
+
+    /* Temporary debug output */
+    struct process *p = plist;
+    while (p) {
+        fprintf(out, "%d:%d:%d:%d\n",
+                p->burst, p->priority, p->arrival, p->queue_id);
+        p = p->next;
+    }
 
     fclose(in);
     fclose(out);
-
     return 0;
 }
